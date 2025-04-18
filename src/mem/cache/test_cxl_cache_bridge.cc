03b1798f9f2fa3d660bd9b84c2a5f6552c7a6205
yuli821 <yuli9@illinois.edu>
2025-04-17 23:12:23 -0500

    testing

--- src/mem/cache/test_cxl_cache_bridge.cc
+++ src/mem/cache/test_cxl_cache_bridge.cc
@@ -1,63 +1,49 @@
 #include "mem/cache/CXLCacheBridge.hh"
-#include "mem/packet.hh"
-#include "sim/clocked_object.hh"
 #include "params/CXLCacheBridge.hh"
-#include "base/logging.hh"
+#include "sim/clocked_object.hh"
+#include "mem/packet.hh"
+#include "sim/init.hh"
+#include "sim/sim_exit.hh"
 #include "base/trace.hh"
 #include <iostream>
 
 using namespace gem5;
 
-namespace {
-
-class DummyParams : public CXLCacheBridgeParams {
-  public:
+class DummyParams : public CXLCacheBridgeParams
+{
+public:
     DummyParams() {
-        name = "cxl_unit_test";
+        name = "cxl_test";
         clock = 1 * SimClock::Int::ns;
         eventq = &mainEventQueue;
     }
 };
 
-void testTranslateToDevice() {
+int main()
+{
+    // Initialize simulation
+    gem5::initSimStats();
+    gem5::initSimObjects();
+
     DummyParams p;
     CXLCacheBridge bridge(p);
 
+    // Translate a host packet
     auto pkt = new Packet(MemCmd::ReadReq, 0x1000);
     bridge.translateToDevice(pkt);
     if (pkt->cmd != MemCmd::ReadSharedReq)
-        panic("ReadReq not translated to ReadSharedReq");
-
-    pkt = new Packet(MemCmd::WriteReq, 0x2000);
-    bridge.translateToDevice(pkt);
-    if (pkt->cmd != MemCmd::ReadUniqueReq)
-        panic("WriteReq not translated to ReadUniqueReq");
+        panic("ReadReq should become ReadSharedReq");
 
     delete pkt;
-}
 
-void testTranslateToHost() {
-    DummyParams p;
-    CXLCacheBridge bridge(p);
+    auto pkt2 = new Packet(MemCmd::WritebackDirty, 0x2000);
+    bridge.translateToHost(pkt2);
+    if (pkt2->cmd != MemCmd::WriteResp)
+        panic("WritebackDirty should become WriteResp");
 
-    auto pkt = new Packet(MemCmd::WritebackDirty, 0x3000);
-    bridge.translateToHost(pkt);
-    if (pkt->cmd != MemCmd::WriteResp)
-        panic("WritebackDirty not translated to WriteResp");
+    delete pkt2;
 
-    pkt = new Packet(MemCmd::CleanEvict, 0x4000);
-    bridge.translateToHost(pkt);
-    if (pkt->cmd != MemCmd::WriteCleanResp)
-        panic("CleanEvict not translated to WriteCleanResp");
-
-    delete pkt;
-}
-
-} // anonymous namespace
-
-int main() {
-    testTranslateToDevice();
-    testTranslateToHost();
-    std::cout << "CXLCacheBridge unit tests passed.\n";
+    std::cout << "CXLCacheBridge unit test passed.\n";
+    exitSimLoop("done", 0);
     return 0;
 }
